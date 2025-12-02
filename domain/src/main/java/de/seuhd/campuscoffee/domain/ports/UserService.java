dd5869160c179fa85da9d664628cb2225fbac297
alezy3 <aleynazeyrek03@gmail.com>
2025-12-02 13:44:27 +0100

    UserService.java

--- domain/src/main/java/de/seuhd/campuscoffee/domain/ports/UserService.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/ports/UserService.java
@@ -2,4 +2,18 @@ package de.seuhd.campuscoffee.domain.ports;
 
 public interface UserService {
     //TODO: Define user service interface
-}
+
+    public interface UserService {
+        
+        List<UserDto> getAllUsers();
+        
+        UserDto getUserbyId(Long id);
+        
+        UserDto getUserbyLoginName(String loginName);
+        
+        UserDto createUser(UserDto user);
+        
+        UserDto updateUser(Long id, UserDto user);
+        
+        void deleteUser(Long id);
+    }
