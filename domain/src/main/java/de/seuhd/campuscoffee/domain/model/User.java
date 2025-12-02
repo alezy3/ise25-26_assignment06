6caa32abb332a8c603bbff63cd098d71044a220d
alezy3 <aleynazeyrek03@gmail.com>
2025-12-02 13:45:34 +0100

    User.java

--- domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/model/User.java
@@ -8,6 +8,25 @@ import java.io.Serializable;
 @Builder(toBuilder = true)
 public record User (
         //TODO: Implement user domain object
+        public class User (
+            private final Long id;
+            private final LocalDateTime createdAt;
+            private final LocalDateTime updatedAt;
+            private final String loginName;
+            private final String emailAddress;
+            private final String firstName;
+            private final String lastName;
+
+        public record User (
+            @Nullable Long id,
+            @Nullable LocalDateTime creeatedAt,
+            @Nullable LocalDateTime updatedAt,
+            @NonNull String loginName,
+            @NonNull String emailAddress,
+            @NonNull String firstName,
+            @NonNull String lastName)    
+            
+        )
 ) implements Serializable { // serializable to allow cloning (see TestFixtures class).
     @Serial
     private static final long serialVersionUID = 1L;
