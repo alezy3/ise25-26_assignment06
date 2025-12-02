7b586472c99d125866368873999315ffe1833548
alezy3 <aleynazeyrek03@gmail.com>
2025-12-02 13:34:59 +0100

    UserDtoMapper.java

--- api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/mapper/UserDtoMapper.java
@@ -2,4 +2,32 @@ package de.seuhd.campuscoffee.api.mapper;
 
 public interface UserDtoMapper {
     //TODO: Implement user DTO mapper
+    
+    public UserDto toDomain(UserDto UserDto) {
+        Objects.requireNonNull(UserDto);
+       
+        return new UserDto(
+            dto.getId(),
+            dto.getCreatedAt(),
+            dto.getUpdatedAt(),
+            dto.getLoginName(),
+            dto.getEmailAddress(),
+            dto.getFirstName(),
+            dto.getLastName(),
+        );     
+    }
+
+    public UserDto toDto(UserDto user) {
+        Objects.requireNonNull (user);
+
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setCreatedAt(user.getCreatedAt());
+        dto.setUpdatedAt(user.getUpdatedAt());
+        dto.setLoginName(user.getLoginName());
+        dto.setEmailAdress(user.getEmailAddress());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+        return dto;
+    }
 }
