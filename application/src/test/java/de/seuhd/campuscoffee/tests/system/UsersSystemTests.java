9f53742fdb54421a6d138f02c52634d41db59f21
alezy3 <aleynazeyrek03@gmail.com>
2025-12-02 13:50:14 +0100

    UsersSystemTests.java

--- application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
+++ application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
@@ -4,14 +4,34 @@ public class UsersSystemTests extends AbstractSysTest {
 
     //TODO: Uncomment once user endpoint is implemented
 
-//    @Test
-//    void createUser() {
-//        User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
-//        User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
-//
-//        assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
-//    }
+    @Test
+    void createUser() {
+        User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
+        User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
+
+        assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
+    }
 
     //TODO: Add at least two additional tests for user operations
+    @Test
+    void getUserbyId() {
+        User existingUser = testFixtures.getUser().get(0);
+        User retrievedUser = userDtoMapper.toDomain(userRequests.getById(existingUser.getId()));
+
+        assertEqualsIgnoringIdAndTimestamps(retrievedUser, existingUser);
+    }
+    
+    @Test
+    void updateUser() {
+        User existingUser = TestFixtures.getUserListForInsertion().get(0);
+        User updatedUserData = TestFixtures.getUserListForInsertion().get(1);
+        User updatedUser = userDtoMapper.toDomain(userRequests.update(existingUser.getId(), userDtoMapper.toDomain(updatedUserData)))
+    }
+
+    @Test
+    void deleteUser() {
+        User existingUser = testFixtures.getUser().get(0);
+        user requests.delete(existingUser.getId());
+    }
 
 }
